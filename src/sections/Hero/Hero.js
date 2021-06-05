b61c07d2f3cdbf2c70857af2363eb80e026d26f6
Hailey <hailey@macbook-pro.myfiosgateway.com>
2021-06-05 18:07:10 -0400

    adding hero section

---
+++ src/sections/Hero/Hero.js
@@ -0,0 +1,27 @@
+import styled  from 'styled-components'
+
+
+const Container = styled.div`
+
+
+
+`
+
+
+const TypeWriter = styled.h1`
+
+
+
+`
+
+const Hero = () =>{
+    return(
+        <section>
+            <Container>
+                <TypeWriter>Web developer who cares.</TypeWriter>
+            </Container>
+        </section>
+    )
+}
+
+export default Hero
